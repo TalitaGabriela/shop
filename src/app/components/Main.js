418e6ec52b3dc544536c42b5f063b6c889136297
TalitaGabriela <talitagabriela1216@gmail.com>
2024-06-07 14:56:58 -0400

    Commit
    aula 07-06-2024

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -1,17 +1,68 @@
+'use client'
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "./main.module.css"
 
-export default async function Main() {
-    const response = await fetch("https://fakestoreapi.com/products/")
-    const data = await response.json();
+export default function Main() {
+
+    const [listProduct, setListProduct] = useState([]);
+
+    useEffect( ()=> {
+      const getProduct = async ()=>{
+        const response = await fetch("https://fakestoreapi.com/products/");
+        const data = await response.json();
+
+        setListProduct(data);
+      }
+
+      getProduct();
+    }, []);
+
+    const ordAz = () => {
+      let newList= [...listProduct].sort( (a,b) =>
+        a.title.localeCompare(b.title)
+      );
+      setListProduct(newList);
+    }
+
+    const ordZa = () => {
+      let newList= [...listProduct].sort( (a,b) =>
+        b.title.localeCompare(a.title)
+      );
+      setListProduct(newList);
+    }
+
+    const  ordBaratCaro = () => {
+      let newList= [...listProduct].sort( (a,b) =>
+        a.price.toString().localeCompare(b.price.toString(),undefined, {numeric:true} )
+      );
+      setListProduct(newList);
+    }
+
+    const ordCaroBarat = () => {
+      let newList= [...listProduct].sort( (a,b) =>
+        b.price.toString().localeCompare(a.price.toString(),undefined, {numeric:true})
+      );
+      setListProduct(newList);
+    }
 
   return (
+    <>
+
+    <div>
+      <button onClick = {ordAz}> A - Z</button>
+      <button onClick = {ordZa}> Z - A</button>
+      <br/>
+      <button onClick = {ordBaratCaro}> Barato - Caro</button>
+      <button onClick = {ordCaroBarat}> Caro - Barato</button>
+    </div>
     <main className={styles.main}>
-      {data.map((products) =>
+    {listProduct.map((products) =>
 
        <div className={styles.card} key={products.id}>
           <p>{products.title}</p>
-          <p>{products.price}</p>
+          <p>{products.price} R$</p>
           <p>{products.description}</p>
           <p>{products.category}</p>
           <p>{products.rating.count}</p>
@@ -20,6 +71,7 @@ export default async function Main() {
           height={100} alt="product image"/>
        </div>
     )};
-       </main>
+       </main> 
+       </>
   );
 }
