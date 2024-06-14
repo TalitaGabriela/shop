ff736e91530aaf13d25836ac08054d3d55ce01e4
TalitaGabriela <talitagabriela1216@gmail.com>
2024-06-14 16:10:43 -0400

    Commit aula 14/06/2024

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -3,75 +3,75 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "./main.module.css"
+import Spinner from "./Spinner";
 
 export default function Main() {
 
-    const [listProduct, setListProduct] = useState([]);
+  const [listProduct, setListProduct] = useState([]);
 
-    useEffect( ()=> {
-      const getProduct = async ()=>{
-        const response = await fetch("https://fakestoreapi.com/products/");
-        const data = await response.json();
+  useEffect(() => {
+    const getProduct = async () => {
+      const response = await fetch("https://fakestoreapi.com/products/");
+      const data = await response.json();
 
-        setListProduct(data);
-      }
-
-      getProduct();
-    }, []);
-
-    const ordAz = () => {
-      let newList= [...listProduct].sort( (a,b) =>
-        a.title.localeCompare(b.title)
-      );
-      setListProduct(newList);
+      setListProduct(data);
     }
 
-    const ordZa = () => {
-      let newList= [...listProduct].sort( (a,b) =>
-        b.title.localeCompare(a.title)
-      );
-      setListProduct(newList);
-    }
+    getProduct();
+  }, []);
 
-    const  ordBaratCaro = () => {
-      let newList= [...listProduct].sort( (a,b) =>
-        a.price.toString().localeCompare(b.price.toString(),undefined, {numeric:true} )
-      );
-      setListProduct(newList);
-    }
+  const ordAz = () => {
+    let newList = [...listProduct].sort((a, b) =>
+      a.title.localeCompare(b.title)
+    );
+    setListProduct(newList);
+  }
 
-    const ordCaroBarat = () => {
-      let newList= [...listProduct].sort( (a,b) =>
-        b.price.toString().localeCompare(a.price.toString(),undefined, {numeric:true})
-      );
-      setListProduct(newList);
-    }
+  const ordZa = () => {
+    let newList = [...listProduct].sort((a, b) =>
+      b.title.localeCompare(a.title)
+    );
+    setListProduct(newList);
 
+  }
+
+  const ordBaratCaro = () => {
+    let newList = [...listProduct].sort((a, b) => a.price - b.price);
+    setListProduct(newList);
+  }
+
+  const ordCaroBarat = () => {
+    let newList = [...listProduct].sort((a, b) => a.price - b.price);
+    newList = newList.reverse();
+    setListProduct(newList);
+  }
+  if (listProduct[0] == null) {
+    return <Spinner />
+  }
   return (
     <>
 
-    <div>
-      <button onClick = {ordAz}> A - Z</button>
-      <button onClick = {ordZa}> Z - A</button>
-      <br/>
-      <button onClick = {ordBaratCaro}> Barato - Caro</button>
-      <button onClick = {ordCaroBarat}> Caro - Barato</button>
-    </div>
-    <main className={styles.main}>
-    {listProduct.map((products) =>
+      <div className={styles.nav}>
+        <button onClick={ordAz} className={styles.button}> A - Z</button>
+        <button onClick={ordZa} className={styles.button}> Z - A</button>
+        <button onClick={ordBaratCaro} className={styles.button}> Barato - Caro</button>
+        <button onClick={ordCaroBarat} className={styles.button}> Caro - Barato</button>
+      </div>
+      <main className={styles.main}>
+        {listProduct.map((products) =>
 
-       <div className={styles.card} key={products.id}>
-          <p>{products.title}</p>
-          <p>{products.price} R$</p>
-          <p>{products.description}</p>
-          <p>{products.category}</p>
-          <p>{products.rating.count}</p>
-          <Image src={products.image} 
-          width={100} 
-          height={100} alt="product image"/>
-       </div>
-    )};
-       </main> 
-       </>
+          <div className={styles.card} key={products.id}>
+            <p className={styles.titulo}>{products.title}</p>
+            <p className={styles.preco}>{products.price} R$</p>
+            <p className={styles.descricao}>{products.description}</p>
+            <p className={styles.categoria}>{products.category}</p>
+            <p className={styles.avaliacao}>{products.rating.count} ESTRELAS</p>
+            <Image src={products.image}
+              width={100}
+              height={100} alt="product image" />
+          </div>
+        )};
+      </main>
+    </>
   );
 }
